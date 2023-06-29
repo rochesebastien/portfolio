f18502661550f7e81961fe8a60a25ae515dc2fd2
sebastien <roche.sebastien@educ-valadon-limoges.fr>
2023-06-30 00:04:55 +0200

    starting new projects design

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,47 +1,36 @@
 import './Projects.scss';
 import { Link } from "react-router-dom";
 import ProjectsData from "../../data/projects.json"
-
-let currentIndex = 0;
-
-function updateCarousel(){
-  const carouselContainer = document.querySelector(".carousel_project");
-  carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
-
-  images.forEach(function(image) {
-    image.classList.remove("dark");
-  });
-
-  images[currentIndex].classList.add("dark");
-}
-
-function listenerCarousel(e){
-  const carouselContainer = document.querySelector(".carousel_project");
-  console.log(carouselContainer);
-  const images = Array.from(carouselContainer.children);
-  const clickedImage = event.target;
-  const clickedIndex = images.indexOf(clickedImage);
-
-  if (clickedIndex !== -1) {
-    currentIndex = clickedIndex;
-    updateCarousel();
-  }
-}
+import { useState } from 'react';
 
 function Projects() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  console.log(currentIndex);
+
+  const handleClick = (index) => {
+    console.log(index);
+    setCurrentIndex(index);
+  };
 
   return (
-    <div className="projects page-container">
-          <span>Page des projets</span>
-          <div className="carousel">
-            <div className="carousel_project">
-            {ProjectsData.map((project) => (
-                    <div  onClick={(e) => listenerCarousel(e)} className="carousel_item" key={project.id}><Link to={`/projects/${project.id-1}`}/>
-                      <img src={`/assets/img/projet${project.id}.jpg`}/>
+    <div className="projects">
+            {ProjectsData.map((project,index) => (
+                    <div className='project-item' onClick={(e) => handleClick(index)} key={project.id}><Link to={`/projects/${project.id-1}`}/>
+                      {/* <img src={`/assets/img/projet${project.id}.jpg`}/> */}
+                      <div className="left">
+                        <img src="/assets/img/noise.png" alt="" />
+                        <span className="name">Projet 0</span>
+                      </div>
+                      <div className="right">
+                        <span className="number">
+                            01
+                        </span>
+                        <span className="lang">
+                          PHP / JS / CSS
+                        </span>
+                      </div>
                     </div>
                 ))}             
-            </div>
-          </div>        
           {/* <span>{ProjectsData.length}</span> */}
       </div>           
   );
