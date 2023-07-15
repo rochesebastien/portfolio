ad11c2d92946b8eb8629138e7b812a0b6c15d9cd
SebNet <sebastienroche230@gmail.com>
2023-07-16 00:37:09 +0200

    title

--- src/components/Projects/ImageProjects.js
+++
@@ -1,9 +0,0 @@
-
-
-function ImageProjects(props) {
-    return (
-        <img src={props.src} alt=""></img>
-    );
-}
-
-export default ImageProjects;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
 import './Home.scss'
 import About from '../About/About'
-import Project from '../Projects/Projects';
+import Projects from '../Projects/Projects';
 
-// import Projects from "./pages/Projects/Projects";
+import Title from '../../components/Title/Title';
 import Skills from "../Skills/Skills";
 import Career from '../Career/Career';
 import Contact from '../Contact/Contact';
@@ -10,14 +10,19 @@ import Contact from '../Contact/Contact';
 function Home() {
     return (
       <div>
-        <video src="/assets/bg.mp4" autoplay loop></video>
+        {/* <video src="/assets/bg.mp4" autoplay loop></video> */}
         <div className="home page-container">
           <img src="/assets/logo.svg" alt="" />
         </div>
+      <Title section_name='Presentation' direction='left'/>
       <About/>
-      <Project/>
+      <Title section_name='Projets' direction='right'/>
+      <Projects/>
+      <Title section_name='Talents' direction='left'/>
       <Skills />
+      <Title section_name='Carrière' direction='right'/>
       <Career />
+      <Title section_name='Contact' direction='left'/>
       <Contact />
     </div>
     );
