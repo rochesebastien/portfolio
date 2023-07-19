e612b514c0de99f22cd4fcff53b49c3694a5e073
sebastien <roche.sebastien@educ-valadon-limoges.fr>
2023-07-20 01:40:53 +0200

    add : cv

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -10,7 +10,7 @@ function Me() {
             <p>
             Mon nom est Roche Sébastien. Possédant un attrait pour l’informatique depuis tout petit, et poussé par ma curiosité, j’ai voulu comprendre comment les jeux de mon enfance fonctionnaient. C’est alors que j’ai découvert le développement web et que j’ai décidé d’en faire mon futur métier. En alliant ma curiosité et ma réflexion, j’ai poursuivi mes études et obtenu un Bachelor CSI option développement logiciel et web, que je compte accompagner d’un Master Développeur Web Full Stack.
             </p>
-            <a href="../../assets/files/cv.pdf" target="_blank">Voir mon CV</a>
+            <a href="/assets/files/cv.pdf" target="_blank">Voir mon CV</a>
         </div>
     </div>
     );
