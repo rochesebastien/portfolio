7d97490bcc02fa8abae55e85be06f329571797bd
rochesebastien <sebastienroche230@gmail.com>
2024-02-22 01:20:41 +0100

    feat : start grid new homepage

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { useState, useEffect } from 'react';
 import Project from "./pages/Projects/Project/Project";
 
 import Layout from "./pages/Layout/Layout";
+import HomePageBackground from "./components/HomePageBackground/HomePageBackground";
 
 function App() {
 
@@ -17,7 +18,7 @@ function App() {
              <Routes> 
               <Route path="/projects/:id" element={<Project />} /> 
               <Route path="" element={<Layout />} /> 
-              <Route path="/test" element={<Layout />} /> 
+              <Route path="/test" element={<HomePageBackground />} /> 
             </Routes> 
       </div>
     )
